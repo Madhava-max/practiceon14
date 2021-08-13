0cbbecab92544df34af75bbda47663a6b8a3babf
kothapalli madhava reddy <85008772+Madhava-max@users.noreply.github.com>
2021-08-13 15:57:19 +0530

    Update linkedlist.cs

--- DS1/linkedlist.cs
+++ DS1/linkedlist.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -73,89 +73,98 @@ namespace DS1
                 
             }
         }
-        //public void delete(int position)
-        //{
-        //    Node temp = head;
-        //    if(position==0)
-        //    {
-        //        head = temp.next;
-        //        Console.WriteLine("the element is deleted which is at the start");
-        //    }
-
-
-        //}
-        //public void deleteAtlast()
-        //{
-        //    Node temp = head;
-        //    Node front = temp.next;
-            
-        //    while(front.next!=null)
-        //    {
-        //        temp = temp.next;
-        //        front = front.next;
-        //    }
-        //    temp.next = null;
-        //}
-        //public int search(int data)
-        //{
-        //    int i = 1;
-        //    Node temp = head;
-        //    while(temp!=null)
-        //    {
-
-
-        //        if (temp.data != data)
-        //        {
-        //            temp = temp.next;
-        //            i++;
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("the position of the value is " + i);
-        //            return i;
-        //        }
-        //    }
-        //    return 0;
-
-        //}
-        public void sorting()
+        public void deleteatfirst()
         {
+            Node temp = head;
+                head = temp.next;
+                Console.WriteLine("the element is deleted which is at the start");
+        }
+        public void deleteAtlast()
+        {
+            Node temp = head;
+            Node front = temp.next;
+
+            while (front.next != null)
             {
+                temp = temp.next;
+                front = front.next;
+            }
+            temp.next = null;
+        }
+        public void delete(int data)
+        {
+            var newNode = new Node(data);
+
+            Node temp = head;
+            Node front = temp.next;
+            while (temp != null)
+            {
+                if(front.data==data)
+                {
+                    temp.next = front.next; ; 
+                }
+                temp = temp.next;
+                front = front.next;
+
 
             }
         }
-        public void searchandinsert(int data1,int data2)
+        public int search(int data)
         {
-            var newNode = new Node(data2);
-            int position = 1;
             int i = 1;
             Node temp = head;
             while (temp != null)
             {
 
 
-                if (temp.data != data1)
+                if (temp.data != data)
                 {
                     temp = temp.next;
                     i++;
                 }
                 else
                 {
-                    //    // Console.WriteLine("the position of the value is " + i);
-                    //    for (position = 1; position <= i; position++)
-                    //    {
-                    //        if (position == i)
-                    //        {
+                    Console.WriteLine("the position of the value is " + i);
+                    return i;
+                }
+            }
+            return 0;
+
+        }
+        public void sorting()
+        {
+            Node temp = head;
+           
+            //Node dummy;
+            while (temp.next != null)
+            {
+                if (temp.data > temp.next.data)
+                {
+                    temp.data = (temp.data) + (temp.next.data);
+                    temp.next.data= (temp.data) - (temp.next.data);
+                    temp.data = (temp.data) - (temp.next.data);
+                }
+                temp = temp.next;
+                
+            }
+        }
+        public void searchandinsert(int data1,int data2)
+        {
+            var newNode = new Node(data2);
+           
+            Node temp = head;
+            while (temp != null)
+            {
+
+
+                if (temp.data == data1)
+                {
                     newNode.next = temp.next;
                     temp.next = newNode;
-                   // break;
-
-
-                    //        }
-                    //    }
-
-                    //}
+                    break;
                 }
+                temp = temp.next;
+           
 
             }
 
